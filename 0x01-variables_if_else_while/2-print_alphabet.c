e6b9d609ade6ed8b8774ebbe769c2cfb71a51b96
bussyadex <bussyadex42@gmail.com>
2022-04-08 16:00:33 -0700

    Done

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*main 
+*
+*Return: Always (Success)
+**/
+int main (void)
+{
+int c;
+for (c='0'; c <='9'; c++)
+{
+putchar(c);
+if (c != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
