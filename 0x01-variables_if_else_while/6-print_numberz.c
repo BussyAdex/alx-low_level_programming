d8030595665b909acc23f0820323992fdae82a3d
bussyadex <bussyadex42@gmail.com>
2022-04-09 00:41:57 -0700

    done

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: This is for the sixth task.
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+
+int n;
+
+for (n = ''0; n <= '9'; n++)
+{
+putchar(n);
+}
+
+putchar('\n');
+
+return (0);
+}
+
