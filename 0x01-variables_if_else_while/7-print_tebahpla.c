d36050086dd165445f0fcb0986326098f9c2bae8
bussyadex <bussyadex42@gmail.com>
2022-04-09 00:48:16 -0700

    done

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: This is for the task 7.
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+
+char c;
+
+for (c = 'z'; c >= 'a'; c--)
+{
+putchar(c);
+}
+
+putchar('\n');
+
+return (0);
+}
+
