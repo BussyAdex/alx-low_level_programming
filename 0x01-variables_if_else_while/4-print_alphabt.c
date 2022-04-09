1e0b32a2097bc8355f9979a9d65ec91af97929d6
bussyadex <bussyadex42@gmail.com>
2022-04-09 00:20:26 -0700

    done

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entrypoint
+ *
+ * Description: This is to fix task 4-print_alphabt.c.
+ * 
+ * Return: Always (Success)
+ */
+int main(void)
+{
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+{
+if (c != 'e' && c != 'q')
+{
+putchar(c);
+}
+}
+putchar('\n');
+
+return (0);
+}
