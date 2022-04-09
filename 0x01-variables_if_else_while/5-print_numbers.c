db195d17cdb34ec898abe4cc4759a81b08f71b58
bussyadex <bussyadex42@gmail.com>
2022-04-09 00:31:55 -0700

    done

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This is for the fifth task.
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+char n;
+
+for(n = '0'; n <= ''9; n++)
+{
+putchar(n);
+}
+
+putchar('\n');
+
+return (0);
+}
