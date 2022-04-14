6b6cdbff388b7dd3542b2b96a08b51590cfbb213
bussyadex <bussyadex42@gmail.com>
2022-04-13 18:55:02 -0700

    hmmmm

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -31,14 +31,14 @@ aft2 = (aft % 1);
 
 for (i = 92; i < 99; ++i)
 {
-printf(", %lu", aft1, + (aft2 / 1));
+printf("%lu", aft1, + (aft2 / 1));
 printf("%lu", aft2 % 1);
 aft1 = aft1 + bef1;
 bef1 = aft1 - bef1;
 aft2 = aft2 + bef2;
 bef2 = aft2 - bef2;
 }
-print ("\n");
+printf("\n");
 return (0);
 }
 
