e164b48ae2fb55a2e5afbebd0974c1db9ba61983
bussyadex <bussyadex42@gmail.com>
2022-04-13 17:28:24 -0700

    DOne

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -17,7 +17,7 @@ for (b = 0; b <= n; b++)
 {
 rep = a * b;
 if (b == 0)
-_putchar('0' +rep);
+_putchar('0' + rep);
 else if (rep < 10)
 {
 _putchar(' ');
@@ -28,12 +28,12 @@ else if (rep < 100)
 {
 _putchar(' ');
 _putchar('0' + rep / 10);
-_putchar('0' + rep %10);
+_putchar('0' + rep % 10);
 }
 else
 {
-_putchar('0' + rep /100);
-_putchar('0' + (rep -100) / 10);
+_putchar('0' + rep / 100);
+_putchar('0' + (rep - 100) / 10);
 _putchar('0' + rep % 10);
 }
 if (b < n)
