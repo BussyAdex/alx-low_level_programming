2c5582b23dac57bae2bf0a583bf0552df6f70eda
bussyadex <bussyadex42@gmail.com>
2022-04-21 08:04:14 -0700

    DO

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -2,14 +2,14 @@
 
 /**
  * puts_half - prints a string, followed by a new line,
- * @stre: pointer to the string to print 
- * Return: void 
+ * Description: Number should be on a line
+ * @str: pointer to the string to print
+ * Return: void
  */
 
 void puts_half(char *str)
 {
 	int len, n, i;
-	
 	len = 0;
 
 	while (str[len] != '\0')
@@ -26,7 +26,7 @@ void puts_half(char *str)
 	}
 	else if (len % 2)
 	{
-		for (n = (len - 1) / 2; n < len -1; n++)
+		for (n = (len - 1) / 2; n < len - 1; n++)
 		{
 			_putchar(str[n + 1]);
 		}
