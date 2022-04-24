91aebd840331377aa6e6cb6cb1f72c1b303d5d14
bussyadex <bussyadex42@gmail.com>
2022-04-24 15:29:25 -0700

    DO

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,19 +8,20 @@
 
 char *_strcat(char *dest, char *src)
 {
-int a;
-int b;
-a = 0;
-while (dest[a] != 0)
-{
-a++;
-}
-b = 0;
-while (src[b] != 0)
-{
-dest[a] = src[b];
-a++;
-b++;
-}
-return (dest);
+	int i = 0, j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
 }
