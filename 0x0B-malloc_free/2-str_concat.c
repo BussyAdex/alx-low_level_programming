daa2256192b6c01b645a69280d3b64bc4ebc0807
bussyadex <bussyadex42@gmail.com>
2022-05-06 17:24:54 -0700

    done

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -21,15 +21,23 @@ char *str_concat(char *s1, char *s2)
 	else
 		s1 = "";
 
-	ret = mallo(size1 + size2 + 1);
+	ptr = s2;
+	if (s2)
+		while (*ptr++)
+			size++;
+	else
+		s2 = "";
+
+	ret = malloc(size1 + size2 + 1);
 	if (!ret)
 		return (NULL);
 
 	ptr = ret;
-	while (s1)
+	while (*s1)
 		*ptr++ = *s1++;
 	while (*s2)
-		*ptr++ = *s1++;
-			*ptr = 0;
+		*ptr++ = *s2++;
+	*ptr = 0;
+
 	return (ret);
 }
